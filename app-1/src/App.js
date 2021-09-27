57c679e7fee90948c9c4bf6bdf0b0dd019b2b7b2
Giovanni Shiroma <giovanni.shiroma04@gmail.com>
2021-09-27 20:41:46 -0300

    commit inicial e inicio header

---
+++ app-1/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header class="headerInicial">
+        <div class="textHeader">
+          <p>uma seleção de produtos</p>
+          <h1>especial para você</h1>
+          <p> todos os produtos desta lista foram selecionados a partir da sua navegação. aproveite!</p>
+        </div>
+        <div class="butHeader">
+          <button class="butStyle">Conheça a linx</button>
+          <button class="butStyle">Ajude o algoritimo</button>
+          <button class="butStyle">Seus produtos</button>
+          <button class="butStyle">Compartilhe </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
